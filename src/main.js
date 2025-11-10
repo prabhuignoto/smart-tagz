927a140e7af342bf44771ff370492cc594964570
prabhuignoto <prabhu.m.murthy@gmail.com>
2025-11-10 16:41:59 +0530

    Implement global styles using SCSS and update main.js to import the new stylesheet

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.scss'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
